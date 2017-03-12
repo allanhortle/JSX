90b570c9fca8fa49a260dff0468bc942cf71f3e2
Allan Hortle <allanhortle@gmail.com>
2017-03-13 09:53:09 +1100

    Fix up comments

--- test/componentTest.jsx
+++ test/componentTest.jsx
@@ -1,46 +1,3 @@
-import classNames from 'classnames';
-import ModalManager from './ModalManager';
-import React from 'react';
-
-class rad extends Other {
-    constructor() {
-
-    }
-}
-
-var ModalManagerView = React.createClass({
-    displayName: 'ModalManagerView',
-    getInitialState() {
-        return {
-            view: null,
-            props: {},
-            data: {}
-        };
-    },
-    componentDidMount() {
-        ModalManager.on('modal:show', this.onShowModal);
-    },
-    onShowModal(data) {
-        this.setState({
-            data: data,
-            view: data.view,
-            props: {
-                onClose: this.onCloseModal,
-                key: 1,
-                ...data.props
-            }
-        });
-    },
-    onCloseModal() {
-        this.setState({view: null, props: null});
-    },
-    onKeyUp(ee) {
-        // ESC
-        if (ee.keyCode === 27) {
-            this.onCloseModal();
-        }
-    },
-
     /*
 
         asdasdasdasd
@@ -55,24 +12,38 @@ var ModalManagerView = React.createClass({
                 : React.cloneElement(this.state.view, this.state.props);
         }
 
+        js jsx
+
+
+        var string = `${steven}: other winner ${radical}`
+        return <div>asdasd</div>;
+
+        return <div> {asdasd} asdasd</div>;
         return (
             <div
                 {...this.props}
                 className={classes}
                 tabIndex="0"
-                {/* dsfkljskadsj;fdklj a;s asdlkfja;sdlkf*/}
                 onRad={ii => <div><div other={null}>{asdas}</div>{asdasd}</div>}
                 onKeyUp={this.onKeyUp}
+                rad={null}
                 ref="modalManager"
-            >
-                    <div className='modal' ref="modal">
-                        <div className="modal_content">
-                            {modal}
-                        </div>
+            ><div>
+                <div className='modal' ref="modal">
+                    <div className="modal_content">
+                        {modal}
                     </div>
+                </div>
             </div>
         );
     }
 });
 
 export default ModalManagerView;
+
+
+function () {
+    return <div></div>;
+}
+
+const rad () => <div></div>;
